63e7ddc82d3cce3d9dc1965f3410144aed4ee657
DocErnstz <ernesto800@outlook.com>
2021-09-27 15:20:33 -0300

    as

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import Navbar from "./section/navbar/navbar";
+import Slider from "./section/slider/slider";
+import Contact from "./section/contact/contact";
+import Footer from "./section/footer/footer";
+import Curve from "./img/Curveimg.png";
+
+function App() {
+  return (
+    <>
+    
+      <Navbar />
+      <Slider /> 
+      <Contact /> 
+      <Footer />
+    </>
+  );
+}
+
+export default App;
